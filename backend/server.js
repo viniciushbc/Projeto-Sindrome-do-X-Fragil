2f4e04c6305c127869ac7b5805ee7fc8725d8985
viniciushbc <vinehbc@gmail.com>
2026-05-15 19:36:27 -0300

    feat: configura conexao inicial com mysql

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ const {testConnection} = require('./src/database/connection.js')
 const PORT = process.env.PORT || 3000;
 
 // quando o banco ficar pronto, tirar o comentario
-// async function startServer(){
-//     await testConnection();
+async function startServer(){
+    await testConnection();
 
 
     app.listen(PORT, ()=>{
         console.log("Rodando na porta: ", PORT)
         console.log(`Documentação da API em: http://localhost:${PORT}/swagger`)
     })
-//}
+}
 
-//startServer();
+startServer();
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,27 +1,27 @@
-// descomentar quando o banco estiver pronto com as credenciais
+//descomentar quando o banco estiver pronto com as credenciais
+require('dotenv').config();
 
+const mysql = require('mysql2/promise');
 
-// const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'sindrome_x_fragil',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST || 'localhost',
-//   port: process.env.DB_PORT || 3306,
-//   user: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASSWORD || '',
-//   database: process.env.DB_NAME || 'sindrome_x_fragil',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+    connection.release();
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error.message);
+  }
+}
 
-// async function testConnection() {
-//   try {
-//     const connection = await pool.getConnection();
-//     console.log('Conexão com o banco de dados estabelecida com sucesso.');
-//     connection.release();
-//   } catch (error) {
-//     console.error('Erro ao conectar com o banco de dados:', error.message);
-//   }
-// }
-
-// module.exports = { pool, testConnection };
+module.exports = { pool, testConnection };
